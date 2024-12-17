279cfcc1a3a9773abd5d60f73b4214f54c731a0f
Zartab Nakhwa <zartab@codewithz.com>
2024-12-17 11:30:13 +0530

    Embedding Expression

--- src/components/counters/Counter.jsx
+++ src/components/counters/Counter.jsx
@@ -2,9 +2,14 @@ import React,{useState} from 'react'
 
 export default function Counter() {
     const [counter]=useState(0)
+
+    function formatCount(){
+        return counter ==0 ? "Zero" : counter
+    }
+
   return (
     <div>
-      <h1>{counter}</h1>
+      <h1>{formatCount()}</h1>
       <button>Increment</button>
     </div>
   )
