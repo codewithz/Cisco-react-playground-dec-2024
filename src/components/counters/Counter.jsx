be14fddc66a834fffa4fe48e837bbae5d00801ff
Zartab Nakhwa <zartab@codewithz.com>
2024-12-17 11:44:07 +0530

    Rendering Classes Dynamically

--- src/components/counters/Counter.jsx
+++ src/components/counters/Counter.jsx
@@ -1,16 +1,22 @@
 import React,{useState} from 'react'
 
 export default function Counter() {
-    const [counter]=useState(0)
+    const [counter]=useState(10)
 
     function formatCount(){
-        return counter ==0 ? "Zero" : counter
+        return counter ===0 ? "Zero" : counter
+    }
+
+    function getBadgeClasses(){
+        let classes= 'badge m-2 '
+        classes+=counter===0?'bg-warning':'bg-primary'
+        return classes;
     }
 
   return (
     <div>
-      <h1>{formatCount()}</h1>
-      <button>Increment</button>
+      <h1 className={getBadgeClasses()}>{formatCount()}</h1>
+      <button className='btn btn-secondary bnt-sm'>Increment</button>
     </div>
   )
 }
