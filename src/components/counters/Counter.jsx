9e2d19391e19f5a92d936c3a7d6f587e60dd9093
Zartab Nakhwa <zartab@codewithz.com>
2024-12-17 11:57:58 +0530

    Rendering List

--- src/components/counters/Counter.jsx
+++ src/components/counters/Counter.jsx
@@ -3,6 +3,8 @@ import React,{useState} from 'react'
 export default function Counter() {
     const [counter]=useState(10)
 
+    const [tags]=useState(['tag1','tag2','tag3'])
+
     function formatCount(){
         return counter ===0 ? "Zero" : counter
     }
@@ -17,6 +19,13 @@ export default function Counter() {
     <div>
       <h1 className={getBadgeClasses()}>{formatCount()}</h1>
       <button className='btn btn-secondary bnt-sm'>Increment</button>
+      <ul>
+        {
+            tags.map((tag)=>(
+                <li key={tag}>{tag}</li>
+            ))
+        }
+      </ul>
     </div>
   )
 }
